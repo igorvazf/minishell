c18222fe3ad21eeeaa47f32a65e405ed90807c86
Igor Fernandes <igorv1286@gmail.com>
2021-11-22 23:30:30 +0100

    add includes

--- include/minishell.h
+++ include/minishell.h
@@ -6,11 +6,21 @@
 /*   By: igvaz-fe <igvaz-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 12:22:10 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/11/08 12:25:10 by igvaz-fe         ###   ########.fr       */
+/*   Updated: 2021/11/22 23:26:37 by igvaz-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
 
 #endif
