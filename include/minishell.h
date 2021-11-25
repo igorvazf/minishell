ef69da4715e8db215176f0ce012df8c5298513fa
Pedro Dias <pedroadias@MacBook-Pro-de-Pedro.local>
2021-11-25 15:56:49 -0300

    adding get_input function

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: paugusto <paugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 12:22:10 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/11/24 21:39:48 by paugusto         ###   ########.fr       */
+/*   Updated: 2021/11/25 15:27:23 by paugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@
 # include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+
+typedef struct s_mini
+{
+	char	*input;
+}	t_mini;
 
 void	minipwd(void);
 void	miniheader(void);
--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,27 @@
 /*   By: paugusto <paugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 12:25:44 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/11/24 21:40:29 by paugusto         ###   ########.fr       */
+/*   Updated: 2021/11/25 15:44:46 by paugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	get_input(t_mini *mini)
+{
+	mini->input = readline(">> ");
+	if (ft_strlen(mini->input) != 0)
+		add_history(mini->input);
+}
+
 int	main(void)
 {
-	char	*buffer;
-	miniheader();
+	t_mini mini;
+
 	while (1)
 	{
-		buffer = readline("> ");
+		get_input(&mini);
+		free(mini.input);
 	}
 	return (0);
 }
