7489da870c3db107f50a0543a5b980783642eb6b
Pedro Dias <psdias.pedro@icloud.com>
2021-12-27 15:08:29 -0300

    fixing norm

--- src/split_cmd.c
+++ src/split_cmd.c
@@ -6,22 +6,16 @@
 /*   By: paugusto <paugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 10:09:19 by paugusto          #+#    #+#             */
-/*   Updated: 2021/12/25 22:43:51 by paugusto         ###   ########.fr       */
+/*   Updated: 2021/12/27 15:04:13 by paugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	split_cmd(t_mini *mini, t_list *list)
+void	dirty_job(t_mini *mini, t_list *list, int i, int start)
 {
 	char	*str;
-	int		i;
-	int		start;
-	int		end;
 
-	i = 0;
-	start = i;
-	end = i;
 	while (mini->input_sanitized[i])
 	{
 		is_in_quote(mini->input_sanitized[i], mini);
@@ -29,8 +23,7 @@ int	split_cmd(t_mini *mini, t_list *list)
 			&& mini->is_open_d == 0)
 		{
 			mini->pipe++;
-			end = i - 1;
-			str = ft_substr(mini->input_sanitized, start, end - start);
+			str = ft_substr(mini->input_sanitized, start, (i - 1) - start);
 			i++;
 			start = i;
 			push_node(list, str);
@@ -39,13 +32,22 @@ int	split_cmd(t_mini *mini, t_list *list)
 		if (mini->input_sanitized[i + 1] == '\0' && mini->is_open_s == 0
 			&& mini->is_open_d == 0)
 		{
-			end = i;
-			str = ft_substr(mini->input_sanitized, start, end - start + 1);
+			str = ft_substr(mini->input_sanitized, start, i - start + 1);
 			push_node(list, str);
 			free(str);
 		}
 		i++;
 	}
+}
+
+int	split_cmd(t_mini *mini, t_list *list)
+{
+	int		i;
+	int		start;
+
+	i = 0;
+	start = i;
+	dirty_job(mini, list, i, start);
 	if (mini->is_open_s == 1 || mini->is_open_d == 1)
 		return (0);
 	return (1);
